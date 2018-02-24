ee6559b5331f3d8e070ddd5303b4f8c444da1924
Chetna1510 <chetna@tychesoftwares.com>
2018-02-24 14:30:19 +0530

    Changed structured of Files.
    Each PHP files will have the structure as per Code Documentation
    Guidelines.

--- product-delivery-date-for-woocommerce-lite/uninstall.php
+++ product-delivery-date-for-woocommerce-lite/uninstall.php
@@ -1,24 +1,21 @@
 <?php
 /**
- * Product Delivery Date Lite Uninstall
+ * Product Delivery Date for WooCommerce - Lite
  *
- * Uninstalling Product Delivery Date Lite delets all settings for the plugin
+ * When Product Delivery Date Lite plugin uninstalls, deletes all settings for the plugin. 
  *
- * @author      Tyche Softwares
- * @category    Core
- * @package     Tyche Softwares/Uninstaller
- * @version     3.2
+ * @author   Tyche Softwares
+ * @package  Product Delivery Date
+ * @since    1.5
+ * @category Core
  */
-
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
-
 global $wpdb;
-
 if ( is_multisite() ) {
     // get main site's table prefix
-    $blog_list = wp_get_sites(  );
+    $blog_list = wp_get_sites();
     foreach( $blog_list as $blog_list_key => $blog_list_value ) {
     	if( $blog_list_value[ 'blog_id' ] > 1 ) {
 			$sub_site_prefix = $wpdb->prefix . $blog_list_value[ 'blog_id' ]."_";
@@ -28,7 +25,6 @@ if ( is_multisite() ) {
             //Minimum Delivery Date 
             $sql_table_post_meta_min_delivery_time = "DELETE FROM `" . $sub_site_prefix . "postmeta` WHERE meta_key='_woo_prdd_lite_minimum_delivery_time'";    
             $wpdb->get_results( $sql_table_post_meta_min_delivery_time );
-
             //Number of Dates to Choose
             $sql_table_post_meta_max_number_days = "DELETE FROM `" . $sub_site_prefix . "postmeta` WHERE meta_key='_woo_prdd_lite_maximum_number_days'";    
             $wpdb->get_results( $sql_table_post_meta_max_number_days );
@@ -37,12 +33,10 @@ if ( is_multisite() ) {
 } else {
 	$sql_table_post_meta = "DELETE FROM `" . $wpdb->prefix . "postmeta` WHERE meta_key='_woo_prdd_lite_enable_delivery_date'";    
     $wpdb->get_results( $sql_table_post_meta );
-
     //Minimum Delivery Date 
     $sql_table_post_meta_min_delivery_time = "DELETE FROM `" . $wpdb->prefix . "postmeta` WHERE meta_key='_woo_prdd_lite_minimum_delivery_time'";    
     $wpdb->get_results( $sql_table_post_meta_min_delivery_time );
-
-            //Number of Dates to Choose
+    //Number of Dates to Choose
     $sql_table_post_meta_max_number_days = "DELETE FROM `" . $wpdb->prefix . "postmeta` WHERE meta_key='_woo_prdd_lite_maximum_number_days'";    
     $wpdb->get_results( $sql_table_post_meta_max_number_days );
 }
