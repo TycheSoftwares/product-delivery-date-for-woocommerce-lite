476e19fa312c0432c149a7e93fa9d688e9e86b26
Pinal <pinalj1612@gmail.com>
2019-03-29 17:49:46 +0530

    Fix #60
    Modified the CSS. This needs to be tested with multiple themes.

--- product-delivery-date-for-woocommerce-lite/product-delivery-date-for-woocommerce-lite.php
+++ product-delivery-date-for-woocommerce-lite/product-delivery-date-for-woocommerce-lite.php
@@ -507,9 +507,9 @@ if ( !class_exists( 'woocommerce_prdd_lite' ) ) {
             }
             print( '<input type="hidden" name="prdd_lite_hidden_minimum_delivery_time" id="prdd_lite_hidden_minimum_delivery_time" value="' . $min_date . '">' );
             if( isset( $prdd_settings ) && $prdd_settings == "on" ) {
-                print ( '<div><label class="delivery_date_label">' . __( "Delivery Date", "woocommerce-prdd-lite" ) . ': </label>
-			    <img src="' . plugins_url() . '/product-delivery-date-for-woocommerce-lite/images/cal.png" width="20" height="20" style="cursor:pointer!important; position:absolute;margin-left:167px;margin-top:10px;" id ="delivery_cal_lite"/>
-                <input type="text" id="delivery_calender_lite" name="delivery_calender_lite" class="delivery_calender_lite" style="cursor:text!important;display:block;  margin-bottom:20px;" readonly/>
+                print ( '<div style="width:70%; position:relative;"><label class="delivery_date_label">' . __( "Delivery Date", "woocommerce-prdd-lite" ) . ': </label>
+			    <img src="' . plugins_url() . '/product-delivery-date-for-woocommerce-lite/images/cal.png" width="20" height="20" style="cursor:pointer!important; position:absolute; top:50%; right:5%;" id ="delivery_cal_lite"/>
+                <input type="text" id="delivery_calender_lite" name="delivery_calender_lite" class="delivery_calender_lite" style="cursor:text!important;display:block;  margin-bottom:20px; width:100%;" readonly/>
                 </div>
                 <input type="hidden" id="prdd_lite_hidden_date" name="prdd_lite_hidden_date"/>
                 <script type="text/javascript">
